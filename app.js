c05df9d4757e8dd8085641c4e9655a1d70eda55d
surajshedge <surajshedge1111@gmail.com>
2024-07-26 19:29:56 +0530

    CORS BC

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ const corsMiddleware = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Adjust this if you need more control
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type, authorization");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type, authorization");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type")
   res.setHeader(
     "Access-Control-Allow-Methods",
     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
